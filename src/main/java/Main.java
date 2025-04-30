7ae57fc987ac9300e831f40b3f5059c589f5a74a
Beskuu <148492037+Beskuu@users.noreply.github.com>
2025-04-30 13:40:40 +0000

    cw5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 /*
 Kod bazowy programu Commit4_0: 
 • Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
@@ -6,19 +7,76 @@ Kod bazowy programu Commit4_0:
 • Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
 • Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
 */
-
+import java.util.Scanner;
 import java.io.IOException;
 
 class Main {
   public static void main(String[] args) {
     try {
+      Scanner skan = new Scanner(System.in);
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      String name;
+      String surName;
+      int day, month, year;
+      int age;
+      int x;
+      while (true) {
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+        System.out.println("Co chciabys zrobić?");
+        System.out.println("1. Dodaj student ");
+        System.out.println("2. Wypisz studentów");
+        System.out.println("3. Wyszukaj studenta po imieniu");
+        System.out.println("0. Wyjście ");
+        System.out.println(" \n");
+        x = skan.nextInt();
+        switch (x) {
+          case 1:
+            System.out.println("Podaj imie: ");
+            name = skan.next();
+            System.out.println("Podaj nazwisko: ");
+            surName = skan.next();
+            System.out.println("Podaj wiek: ");
+            age = skan.nextInt();
+            System.out.println("Podaj date urodzenia: ");
+            day = skan.nextInt();
+
+            if (day < 1 || day > 31) {
+              System.out.println("Podaj poprawny dzien urodzenia : ");
+              day = skan.nextInt();
+            }
+            System.out.println("Podaj miesiac urodzenia: ");
+            month = skan.nextInt();
+            if (month < 1 || month > 12) {
+              System.out.println("Podaj poprawny miesiac urodzenia: ");
+              month = skan.nextInt();
+            }
+            System.out.println("Podaj rok urodzenia: ");
+            year = skan.nextInt();
+            if (year < 1 || year > 2025) {
+              System.out.println("Podaj poprawny rok urodzenia: ");
+              year = skan.nextInt();
+            }
+            Student s1 = new Student(name, surName, age, day, month, year);
+            s.addStudent(s1);
+            break;
+          case 2:
+
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.ToString());
+            }
+            break;
+          case 3:
+            System.out.println("Wyszukaj po imieniu: ");
+            name = skan.next();
+            Student s2 = s.findStudentByName(name);
+            System.out.println(s2.ToString());
+            if (s2 == null)
+              System.out.println("Nie znaleziono studenta o imieniu " + name);
+            break;
+          case 0:
+            System.exit(0);
+        }
       }
     } catch (IOException e) {
 
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,23 +2,54 @@ public class Student {
 
   private String Name;
   private int Age;
+  private String SurName;
+  private int Day;
+  private int Month;
+  private int Year;
 
-  public Student(String name, int age) {
+  public Student(String name, String surName, int age, int day, int month, int year) {
     Name = name;
+    SurName = surName;
     Age = age;
+    Day = day;
+    Month = month;
+    Year = year;
   }
 
-  public String GetName() {return Name;}
-  public int GetAge() {return Age;}
+  public String GetName() {
+    return Name;
+  }
+
+  public String GetSurName() {
+    return SurName;
+  }
+
+  public int GetAge() {
+    return Age;
+  }
+
+  public int GetDay() {
+    return Day;
+  }
+
+  public int GetMonth() {
+    return Month;
+  }
+
+  public int GetYear() {
+    return Year;
+  }
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + SurName + " " + Integer.toString(Age) + " " + Integer.toString(Day) + " "
+        + Integer.toString(Month) + " " + Integer.toString(Year);
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if (data.length != 6)
+      return new Student("Parse", "Error", -1, -1, -1, 9999);
+    return new Student(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]),
+        Integer.parseInt(data[4]), Integer.parseInt(data[5]));
   }
 }
